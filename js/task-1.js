361b7327d3e34f3ba30d97597b120a8893900696
Andriy Muzyka <andripikus@gmail.com>
2024-05-16 20:33:59 +0200

    hw-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,10 @@
 function isEnoughCapacity(products, containerSize) {
   let totalQuantity = 0;
-
-  for (const quantity of Object.values(products)) {
-    totalQuantity += quantity;
+  for (const product in products) {
+    if (products.hasOwnProperty(product)) {
+      totalQuantity += products[product];
+    }
   }
-
   return totalQuantity <= containerSize;
 }
 
